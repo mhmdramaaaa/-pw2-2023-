fdb4b36e0a448f88eec117e267f872549c60d965
mhmdramaaaa <ramadani9743@gmail.com>
2024-03-25 20:23:31 +0700

    upload pertemuan5

--- pertemuan 5/Tugas/data_kasus.php
+++ pertemuan 5/Tugas/data_kasus.php
@@ -1,18 +1,17 @@
 <?php
 
-// Include file class_kasus.php
+
 include 'class_kasus.php';
 
-// Buat objek baju
+
 $baju = new Baju("Kaos", "M", "Hitam", "Uniqlo");
 
-// Buat objek celana
+
 $celana = new Celana("Jeans", "42", "Slim Fit", "Levi's");
 
-// Buat objek jaket
 $jaket = new Jaket("coach", "XL", "soft parasut", "Erigo");
 
-// Tampilkan informasi pakaian
+
 echo "Informasi Baju:<br> " . $baju->getInfo() . "<br>";
 echo "<br>";
 
